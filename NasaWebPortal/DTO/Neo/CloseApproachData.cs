ec7212edd5e1b8077172f1e9b63d020183dfdc23
Matthew Martin <matthew-martin@programmer.net>
2023-05-20 12:45:09 -0400

    Fixed According

--- NasaWebPortal/DTO/Neo/CloseApproachData.cs
+++ NasaWebPortal/DTO/Neo/CloseApproachData.cs
@@ -4,13 +4,14 @@ namespace NasaWebPortal.DTO.Neo
 {
     public class CloseApproachData
     {
+        [JsonPropertyName("close_approach_date")]
+        public string ApproachId { get; set; }
         [JsonPropertyName("orbiting_body")]
         public string OrbitingBody { get; set; }
         [JsonPropertyName("relative_velocity")]
         public RelativeVelocity RelativeVelocity { get; set; }
         [JsonPropertyName("miss_distance")]
         public MissDistance MissDistance { get; set; }
-        public bool IsVisable { get; set; } = false;
 
     }
 }
